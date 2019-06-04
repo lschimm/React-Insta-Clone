49612aa1af05ce23ab102c2c073f3d000e6a9717
lschimm <lschimmelfennig@gmail.com>
2019-06-03 22:08:45 -0500

    finished styling and adding all parts

--- instagram-app/src/components/CommentSection/Comments.js
+++ instagram-app/src/components/CommentSection/Comments.js
@@ -6,18 +6,29 @@ const Comments = props => {
     return(
         <div className="comments">
             <div className="userAnd">
-            <img src={props.thePosts.thumbnailUrl} 
-            className="thumbnail"
-            />
-            <h5 className="username">{props.thePosts.username}</h5>
+                <img src={props.thePosts.thumbnailUrl} 
+                className="thumbnail" />
+                <h5 className="username">{props.thePosts.username}</h5>
             </div>
-            <img src={props.thePosts.imageUrl}
-            />
-            <div className="likesEtc">
-            <p>{props.thePosts.likes} likes</p>
+            <img src={props.thePosts.imageUrl} />
+            
+            <div className="likes">
+                <button className="button">like</button>
+                <button className="button">cmnt</button>
+                <p>{props.thePosts.likes} likes</p>
+            </div>
+            <div className="user">
+                <p>{props.thePosts.comments[0].username}
+                {props.thePosts.comments[0].text}</p>
+                <p>{props.thePosts.comments[1].username}
+                {props.thePosts.comments[1].text}</p>
+                <p>{props.thePosts.comments[2].username}
+                {props.thePosts.comments[2].text}</p>
+            </div>
+            <div className="date">
             <p>{props.thePosts.timestamp}</p>
-            <p>{props.thePosts.comments[0].text}</p>
             </div>
+
             {/* <props.thePosts.map(thepost =>) */}
         </div>
     )
